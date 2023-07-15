6945f01838f638a4a34b7e6948c25fb40ec17295
Osha Foster <31053423+OshaFoster@users.noreply.github.com>
2023-07-15 10:24:55 -0600

    small fix

--- components/About/index.js
+++ components/About/index.js
@@ -12,11 +12,11 @@ export default function About() {
           with a team, and I work well solo. I am a
           <Span>&nbsp;good thinker</Span>, ask thoughtful questions, and quickly
           identify the root of a problem. I
-          <Span>&nbsp;love learning&nbsp;</Span> new skills and excel at
+          <Span>&nbsp;love learning&nbsp;</Span>new skills and excel at
           enhancing the user experience. I have contributed to the development
           of seven apps, utilizing four different technology stacks, and
           overseeing the entire process from from initial concept to deployment.
-          I am excited to work with you.
+          <Span>&nbsp;I am excited to work with you.&nbsp;</Span>
         </Text>
       </TextWrap>
     </>
