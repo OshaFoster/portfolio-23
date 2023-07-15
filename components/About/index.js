ea2fdb57ca82bc49cd60677d84d22e83d5d18c80
Osha Foster <31053423+OshaFoster@users.noreply.github.com>
2023-07-15 13:17:43 -0600

    update about

--- components/About/index.js
+++ components/About/index.js
@@ -14,8 +14,9 @@ export default function About() {
           identify the root of a problem.
           <Span>&nbsp;I love learning new skills&nbsp;</Span>and excel at
           enhancing the user experience. I have contributed to the development
-          of seven apps, utilizing four different technology stacks, and
-          overseeing the entire process from initial concept to deployment.
+          of apps in industries such as medical supply, construction and data
+          analysis. Using different technology stacks and overseeing the entire process from
+          initial concept to deployment.
           <Span>&nbsp;I am excited to work with you.&nbsp;</Span>
         </Text>
       </TextWrap>
